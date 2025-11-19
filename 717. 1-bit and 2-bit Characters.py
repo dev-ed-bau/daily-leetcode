042da1966e058ae15a370d2b3425c42ed53a6fee
Eduardo Bautista <eduardobautistauribe7@gmail.com>
2025-11-18 23:53:07 -0800

    717

---
+++ 717. 1-bit and 2-bit Characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isOneBitCharacter(self, bits: List[int]) -> bool:
+        i = 0
+
+        while i < len(bits) - 1:
+            if bits[i] == 0:
+                i += 1
+            else:
+                i += 2
+
+        print(i)
+
+        if i == len(bits) - 1:
+            return True
+            
+        return False
+            
